520d8ba608c6ab89407c9f589b59336b3ed187c5
Rayen <78444938+rayenkorbi@users.noreply.github.com>
2024-03-27 13:43:17 +0100

    27/03/2024 : Version 0.03
    changes:
    -added right click menu (not functional yet)
    -added google that is working

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ function toggleMenu() {
 }
 
 /* Popup open / close */
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var softwareIcons = document.querySelectorAll('.software');
 
-    softwareIcons.forEach(function(icon) {
-        icon.addEventListener('click', function() {
-            document.querySelectorAll('.popup').forEach(function(popup) {
+    softwareIcons.forEach(function (icon) {
+        icon.addEventListener('click', function () {
+            document.querySelectorAll('.popup').forEach(function (popup) {
                 popup.style.display = 'none';
             });
             this.querySelector('.popup').style.display = 'block';
@@ -22,12 +22,47 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     var closeButtons = document.querySelectorAll('.close-btn');
-    closeButtons.forEach(function(btn) {
-        btn.addEventListener('click', function(e) {
-            e.stopPropagation(); 
-            this.parentElement.style.display = 'none'; 
+    closeButtons.forEach(function (btn) {
+        btn.addEventListener('click', function (e) {
+            e.stopPropagation();
+            this.parentElement.style.display = 'none';
         });
     });
 });
 
+
+/*RightClick Show / Hide */
+document.addEventListener("contextmenu", function (event) {
+    event.preventDefault();
+    var contextMenu = document.getElementById("rightClick");
+    var menuWidth = contextMenu.offsetWidth;
+    var menuHeight = contextMenu.offsetHeight;
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+
+    //inside the viewport
+    var posX = event.clientX;
+    var posY = event.clientY;
+
+    //right edge
+    if (posX + menuWidth > windowWidth) {
+        posX = windowWidth - menuWidth;
+    }
+
+    //bottom edge
+    if (posY + menuHeight > windowHeight) {
+        posY = windowHeight - menuHeight;
+    }
+
+    contextMenu.style.top = posY + "px";
+    contextMenu.style.left = posX + "px";
+    contextMenu.classList.add("active");
+});
+
+document.addEventListener("click", function (event) {
+    var rightClickMenu = document.getElementById("rightClick");
+    if (!rightClickMenu.contains(event.target)) {
+        rightClickMenu.classList.remove("active");
+    }
+});
 
